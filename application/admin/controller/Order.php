2bef0442e68a379ed326d34596937d9f99513f40
weirdo <weirdolan2411@gmail.com>
2019-11-19 10:40:03 +0800

    增加后台修改订单状态操作

--- application/admin/controller/Order.php
+++ application/admin/controller/Order.php
@@ -29,8 +29,8 @@ class Order extends Base {
 			}
 
 			$redis = new Redis();
-			$redis->get($orderInfo['id']) && showMsg('操作失败,'.$redis->ttl($orderInfo['id'])."秒后可操作", 0);
-			$redis->set($orderInfo['id'], TRUE, 60);
+			$redis->get($orderInfo['id']) && showMsg('操作失败,' . $redis->ttl($orderInfo['id']) . "秒后可操作", 0);
+			$redis->set($orderInfo['id'], TRUE, 30);
 
 			Db::startTrans();
 			$res1 = Db::name('order_buy')->where('id=' . $edit)->update($updateArr); // 更新订单
@@ -41,7 +41,7 @@ class Order extends Base {
 				!in_array($orderInfo['status'], ['5', '9']) && showMsg('该状态不能重建订单', 0);
 				//在余额里面进行扣钱
 				$realAmt = $orderInfo['deal_num'] + $orderInfo['fee'];
-				$res2 = balanceChange(TRUE, $orderInfo['sell_id'], -$orderInfo['deal_num'], $orderInfo['fee'], $orderInfo['deal_num'], $orderInfo['fee'], BAL_SYS, $orderInfo['id'], "重建订单");
+				$res2    = balanceChange(TRUE, $orderInfo['sell_id'], -$orderInfo['deal_num'], $orderInfo['fee'], $orderInfo['deal_num'], $orderInfo['fee'], BAL_SYS, $orderInfo['id'], "重建订单");
 				//$res2    = Db::name('merchant')->where(['id' => $orderInfo['sell_id']])->setDec('usdt', $realAmt);
 				//$res3    = Db::name('merchant')->where(['id' => $orderInfo['sell_id']])->setInc('usdtd', $realAmt);
 			}
@@ -109,7 +109,7 @@ class Order extends Base {
 					}
 					// $rs2 = Db::table('think_order_buy')->update(['id'=>$orderinfo['id'], 'status'=>4, 'finished_time'=>time(), 'platform_fee'=>$moneyArr[0]]);
 					//$rs3      = Db::table('think_merchant')->where('id', $orderInfo['buy_id'])->setInc('usdt', $mum);
-					$rs3 = balanceChange(TRUE, $orderInfo['buy_id'], $mum, 0, 0, 0, BAL_BOUGHT, $orderInfo['id'], "编辑修改状态->完成订单");
+					$rs3      = balanceChange(TRUE, $orderInfo['buy_id'], $mum, 0, 0, 0, BAL_BOUGHT, $orderInfo['id'], "编辑修改状态->完成订单");
 					$rs4      = Db::table('think_merchant')->where('id', $orderInfo['sell_id'])->setInc('transact', 1);
 					$total    = Db::table('think_order_buy')->field('sum(finished_time-dktime) as total')->where('sell_id', $orderInfo['sell_id'])->where('status', 4)->select();
 					$tt       = $total[0]['total'];
@@ -118,7 +118,7 @@ class Order extends Base {
 					//承兑商卖单奖励
 					$rs6 = $rs7 = $rs8 = $rs9 = $rs10 = $rs11 = $res3 = TRUE;
 					if ($traderMoney > 0) {
-						$rs6 = balanceChange(TRUE, $orderInfo['sell_id'], $traderMoney, 0,0, 0, BAL_COMMISSION, $orderInfo['id'], "编辑修改状态->完成订单");
+						$rs6 = balanceChange(TRUE, $orderInfo['sell_id'], $traderMoney, 0, 0, 0, BAL_COMMISSION, $orderInfo['id'], "编辑修改状态->完成订单");
 						//$rs6 = Db::table('think_merchant')->where('id', $orderInfo['sell_id'])->setInc('usdt', $traderMoney);
 						$rs7 = Db::table('think_trader_reward')->insert(['uid' => $orderInfo['sell_id'], 'orderid' => $orderInfo['id'], 'amount' => $traderMoney, 'type' => 0, 'create_time' => time()]);
 					}
@@ -166,33 +166,56 @@ class Order extends Base {
 					Db::rollback();
 					showMsg('释放失败，参考信息：' . $e->getMessage(), 0);
 				}
-			} else {
-				// 判断取消订单
-				if ($args['status'] == 5 && ($src_status == 0 || $src_status == 1)) {
-					// 减少挂卖单交易量和增加挂卖单剩余量
-					$real_number = $orderInfo['deal_num'] + $orderInfo['fee'];
-					$res4        = Db::name('ad_sell')->where(['id' => $orderInfo['sell_sid']])->setDec('trading_volume', $real_number);
-					$res5        = Db::name('ad_sell')->where(['id' => $orderInfo['sell_sid']])->setInc('remain_amount', $real_number);
-					// 获取挂单
-					$sell = Db::name('ad_sell')->where('id', $orderInfo['sell_sid'])->find();
-					if ($sell['state'] == 2) {
-						// 如果挂单已下架 回滚余额
-						$res6 = balanceChange(TRUE, $orderInfo['sell_id'], $orderInfo['deal_num'], $orderInfo['fee'], -$orderInfo['deal_num'], $orderInfo['fee'], BAL_CANCEL, $orderInfo['id'], "编辑修改状态->取消订单");
+			}
+			// 判断取消
+			if ($args['status'] == 5 && ($src_status == 0 || $src_status == 1)) {
+				// 减少挂卖单交易量和增加挂卖单剩余量
+				$real_number = $orderInfo['deal_num'] + $orderInfo['fee'];
+				$res4        = Db::name('ad_sell')->where(['id' => $orderInfo['sell_sid']])->setDec('trading_volume', $real_number);
+				$res5        = Db::name('ad_sell')->where(['id' => $orderInfo['sell_sid']])->setInc('remain_amount', $real_number);
+				// 获取挂单
+				$sell = Db::name('ad_sell')->where('id', $orderInfo['sell_sid'])->find();
+				if ($sell['state'] == 2) {
+					// 如果挂单已下架 回滚余额
+					$res6 = balanceChange(TRUE, $orderInfo['sell_id'], $orderInfo['deal_num'], $orderInfo['fee'], -$orderInfo['deal_num'], $orderInfo['fee'], BAL_CANCEL, $orderInfo['id'], "编辑修改状态->取消订单");
 
-						//$res6 = Db::name('merchant')->where('id', $orderInfo['sell_id'])->setInc('usdt', $real_number);
-						//$res7 = Db::name('merchant')->where('id', $orderInfo['sell_id'])->setDec('usdtd', $real_number);
-					}
-					// 减少用户冻结余额和增加用户余额
-					//$res2    = Db::name('merchant')->where(['id' => $orderInfo['sell_id']])->setInc('usdt', $orderInfo['deal_num']);
-					//$res3    = Db::name('merchant')->where(['id' => $orderInfo['sell_id']])->setInc('usdtd', $orderInfo['deal_num']);
+					//$res6 = Db::name('merchant')->where('id', $orderInfo['sell_id'])->setInc('usdt', $real_number);
+					//$res7 = Db::name('merchant')->where('id', $orderInfo['sell_id'])->setDec('usdtd', $real_number);
 				}
-				if ($res1 && $res2 && $res3 && $res4 && $res5 && $res6 && $res7) {
-					Db::commit();
-					showMsg('操作成功', 1);
-				} else {
-					Db::rollback();
-					showMsg('操作失败', 0);
+				// 减少用户冻结余额和增加用户余额
+				//$res2    = Db::name('merchant')->where(['id' => $orderInfo['sell_id']])->setInc('usdt', $orderInfo['deal_num']);
+				//$res3    = Db::name('merchant')->where(['id' => $orderInfo['sell_id']])->setInc('usdtd', $orderInfo['deal_num']);
+			}
+			// 判断付款&放行
+			if ($src_status == 5 && ($args['status'] == 0 || $args['status'] == 1)) {
+				// 关闭状态下改待付款或待放行
+				$real_number = $orderInfo['deal_num'] + $orderInfo['fee'];
+				// 如果挂单已下架则不允许修改
+				$sellInfo = Db::name('ad_sell')->where('id', $orderInfo['sell_sid'])->where('state', 2)->find();
+				$sellInfo && showMsg('原挂单已下架，不允许修改订单状态。', 0);
+				// 剩余数量不足
+				$sellInfo['remain_amount'] < $real_number && showMsg('挂单剩余数量不足，无法修改。', 0);
+				/*
+				if ($sellinfo['remain_amount'] < $real_number) {
+					// 如果账户余额不足则不操作
+					$sellUser = Db::name('merchant')->where('id', $orderInfo['sell_id'])->find();
+					$sellUser['usdt'] < $real_number && showMsg('账户可用余额不足，无法修改。', 0);
+					// 如果余额足够 则直接扣余额
+					!balanceChange(TRUE, $orderInfo['sell_id'], -$real_number, 0, $real_number, 0, BAL_SYS, $orderInfo['id'], "挂单剩余数量不足") && showMsg('扣除余额失败', 0);
 				}
+				*/
+				// 需重新扣除剩余数量
+				$res5 = Db::name('ad_sell')->where(['id' => $orderInfo['sell_sid']])->setDec('remain_amount', $real_number);
+				$res4 = Db::name('ad_sell')->where(['id' => $orderInfo['sell_sid']])->setInc('trading_volume', $real_number);
+			}
+			if ($res1 && $res2 && $res3 && $res4 && $res5 && $res6 && $res7) {
+				Db::commit();
+				$redis->rm($orderInfo['id']);
+				showMsg('操作成功', 1);
+			} else {
+				Db::rollback();
+				$redis->rm($orderInfo['id']);
+				showMsg('操作失败', 0);
 			}
 		}
 		$this->assign('data', $orderInfo);
