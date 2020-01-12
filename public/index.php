b2f56e06ecaf570b4b1f2b5253116218198d0e71
Aston <aston.seal@gmail.com>
2020-01-12 08:56:59 +0800

    美化一下

--- public/index.php
+++ public/index.php
@@ -1,28 +1,13 @@
 <?php
-// +----------------------------------------------------------------------
-// | ThinkPHP [ WE CAN DO IT JUST THINK ]
-// +----------------------------------------------------------------------
-// | Copyright (c) 2006-2015 http://thinkphp.cn All rights reserved.
-// +----------------------------------------------------------------------
-// | Licensed ( http://www.apache.org/licenses/LICENSE-2.0 )
-// +----------------------------------------------------------------------
-// | Author: liu21st <liu21st@gmail.com>
-// +----------------------------------------------------------------------
-
-// [ 应用入口文件 ]
 // 定义应用目录
-define('APP_PATH', __DIR__ . '/../application/');
-// 定义上传目录
-define('UPLOAD_PATH', __DIR__ . '/../public');
-// 定义应用缓存目录
-define('RUNTIME_PATH', __DIR__ . '/../runtime/');
-// 开启调试模式
-define('APP_DEBUG', false);
-
-define('ADMIN_KEY', '123Abc');
+const APP_PATH     = __DIR__ . '/../application/';
+const UPLOAD_PATH  = __DIR__ . '/../public';   // 定义上传目录
+const RUNTIME_PATH = __DIR__ . '/../runtime/'; // 定义应用缓存目录
+const APP_DEBUG    = FALSE;                    // 开启调试模式
+const ADMIN_KEY    = '123Abc';
 error_reporting(0);
 ini_set('display_errors', '0');
-if(APP_DEBUG){
+if (APP_DEBUG) {
 	ini_set('display_errors', '1');
 	error_reporting(-1);
 }
