64376f64d0e9dc5b3d6556bd00d8e45a9e014d4e
Aston <aston.seal@gmail.com>
2020-01-28 10:07:10 +0800

    修改会话超时问题

--- public/index.php
+++ public/index.php
@@ -11,12 +11,13 @@ if (APP_DEBUG) {
 	ini_set('display_errors', '1');
 	error_reporting(-1);
 }
-ini_set('session.auto_start', 1);
+/*ini_set('session.auto_start', 1);
 ini_set('session.save_handler', 'redis');
 ini_set('session.name', 'sid');
+ini_set('session.save_path', 'tcp://127.0.0.1:6379?weight=1&read_timeout=3');
 ini_set('session.save_path', 'tcp://127.0.0.1:6379?weight=1&timeout=3&read_timeout=3&persistent=1');
 ini_set('session.gc_maxlifetime', 5184000);
 ini_set('session.cookie_lifetime', 5184000);
-ini_set('session.serialize_handler ', 'php');
+ini_set('session.serialize_handler ', 'php');*/
 // 加载框架引导文件
 require __DIR__ . '/../vendor/thinkphp/start.php';
