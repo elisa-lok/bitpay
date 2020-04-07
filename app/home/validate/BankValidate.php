bc9fbf2297a57e04a1ea9eed80655988c4c8601b
Elisa <elisalok413@gmail.com>
2020-04-07 16:14:00 +0800

    去掉标识

--- app/home/validate/BankValidate.php
+++ app/home/validate/BankValidate.php
@@ -4,16 +4,15 @@ use think\Validate;
 
 class BankValidate extends Validate {
 	protected $rule    = [
-			'name'          => 'require|max:50|unique:merchant_bankcard',
-			'c_bank'        => 'require',
-			'c_bank_detail' => 'require',
+		'name'          => 'require|max:50',
+		'c_bank'        => 'require',
+		'c_bank_detail' => 'require',
 	];
 	protected $message = [
-			'name.require'          => '银行卡标识不能为空',
-			'name.max'              => '银行卡标识最多不能超过50个字符',
-			'name.unique'           => '银行卡标识已存在',
-			'c_bank.require'        => '开户银行不能为空',
-			'c_bank_detail.require' => '开户支行不能为空',
+		'name.require'          => '银行卡标识不能为空',
+		'name.max'              => '银行卡标识最多不能超过50个字符',
+		'c_bank.require'        => '开户银行不能为空',
+		'c_bank_detail.require' => '开户支行不能为空',
 	];
 }
 
