0bd604e86bba7fa663b9621589b518dde9ec96c5
Elisa <elisalok413@gmail.com>
2020-04-19 22:53:31 +0800

    添加支付方式查看

--- app/admin/controller/Payment.php
+++ app/admin/controller/Payment.php
@@ -1,16 +1,21 @@
 <?php
 namespace app\admin\controller;
+// 查看用户支付方式
 use think\Db;
 
-// 查看用户支付方式
 class Payment extends Base {
 	public function index() {
-		$id   = input('id');
-		$user = Db::name('merchant')->where('id', $id)->find();
-		showMsg('', 0, ['usdt' => $user['usdt'], 'usdtd' => $user['usdtd'], 8]);
-	}
-
-	public function get() {
-		$args = input();
+		$tag    = input('tag');
+		$payWay = input('pay_way');
+		$payWay = $payWay === NULL ? 1 : (int)$payWay;
+		$data = [];
+		if ($tag !== NULL) {
+			$payWayTbl = [0 => 'merchant_bankcard', 1 => 'merchant_zfb', 2 => 'merchant_wx', 3 => 'merchant_ysf'];
+			$data = Db::name($payWayTbl[$payWay])->where(['merchant_id|name|c_bank|truename|c_bank_detail'=> ['like', '%' . $tag . '%']])->limit(50)->order('state DESC')->select();
+		}
+		$this->assign('tag', $tag);
+		$this->assign('pay_way', $payWay);
+		$this->assign('data', $data);
+		return $this->fetch();
 	}
 }
